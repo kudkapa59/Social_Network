c9310465e1060909da44c35e5f0fad347edf90d1
Kapa Kudaibergenov <66877169+kudkapa59@users.noreply.github.com>
2023-01-16 11:53:01 +0100

    Git project creation. Added containers for Dialogues and Profile pages.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Profile from './components/Profile';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import { Route } from 'react-router-dom';
+import DialoguesContainer from './components/Dialogues/DialoguesContainer';
 
-const App = () => {
+const App = (props) => {
+    let state
+
   return (
     <div className='app-wrapper'>
       <Header />
-      <Navbar />
-      <Profile />
+      <Navbar state={props.store.getState().sidebar} />
+      <div className='app-wrapper-content'>
+        {/* <Route path="/dialogues" component={Dialogues} />
+        <Route path="/profile" component={Profile} /> */}
+        <Route path="/dialogues" render={() => <DialoguesContainer store={props.store}/>} />
+        <Route path="/profile" render={() => <Profile store={props.store}/>} />
+        <Route path="/news" />
+        <Route path="/music" />
+        <Route path="/settings" />
+      </div>
     </div>);
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './redux/redux-store';
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+export let rerenderEntireTree = (state) => {
+    ReactDOM.render(<BrowserRouter><App store={store}/>
+    </BrowserRouter>, document.getElementById('root'))
+}
+
+rerenderEntireTree(store.getState())
+
+store.subscribe(() => {
+    rerenderEntireTree(store.getState())
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
