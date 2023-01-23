d9d6b84eafca70e201bf3d7d234310a6ae88c866
Kapa Kudaibergenov <66877169+kudkapa59@users.noreply.github.com>
2023-01-23 17:02:26 +0100

    #55 Added pagination to Users page.

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Users from "./Users";
 import {connect} from "react-redux";
-import {followAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
+import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
 
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        pageSize: state.usersPage.pageSize,
+        currentPage: state.usersPage.currentPage
     }
 }
 
@@ -14,7 +17,9 @@ let mapDispatchToProps = (dispatch) => {
     return {
         follow: (userId) => {dispatch(followAC(userId))},
         unfollow: (userId) => {dispatch(unfollowAC(userId))},
-        setUsers: (users) => {dispatch(setUsersAC(users))}
+        setUsers: (users) => {dispatch(setUsersAC(users))},
+        setCurrentPage: (pageNumber) => {dispatch(setCurrentPageAC(pageNumber))},
+        setTotalUsersCount: (usersCount) => {dispatch(setTotalUsersCountAC(usersCount))}
     }
 }
 
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,9 +1,14 @@
 const FOLLOW = 'FOLLOW' //action types
 const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
 
 let initialState = {
-    users: []
+    users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1
 }
 
 const usersReducer = (state=initialState, action) => {
@@ -30,8 +35,12 @@ const usersReducer = (state=initialState, action) => {
             }
         case SET_USERS:
             return {
-                ...state, users: [...state.users, ...action.users]
+                ...state, users: [...action.users] //users only for this page
             }
+        case SET_CURRENT_PAGE:
+            return {...state, currentPage: action.pageNumber}
+        case SET_TOTAL_USERS_COUNT:
+            return {...state, totalUsersCount: action.totalUsersCount}
         default:
             return state
     }
@@ -45,6 +54,12 @@ export const unfollowAC = (userId) => ({
 })
 export const setUsersAC = (users) => ({
     type: SET_USERS, users
+})
+export const setCurrentPageAC = (pageNumber) => ({
+    type: SET_CURRENT_PAGE, pageNumber
+})
+export const setTotalUsersCountAC = (totalUsersCount) => ({
+    type: SET_TOTAL_USERS_COUNT, totalUsersCount
 })
 
 export default usersReducer
